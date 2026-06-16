5aaa991805e2546677ed026159029380083458ef
Abhiranjan Tiwary <abhiranjantiwary.cse@gmail.com>
2026-06-16 17:57:14 +0530

    Latest project updates

--- server/server.js
+++ server/server.js
@@ -18,6 +18,7 @@ dotenv.config();
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+dotenv.config({ path: path.join(__dirname, ".env") });
 
 const app = express();
 const server = http.createServer(app);
